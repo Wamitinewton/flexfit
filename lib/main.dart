c84abe22be1542e053b453441921a16eb6b3764b
Wamitinewton <wamitinewton@gmail.com>
2024-05-08 17:59:55 +0300

    n

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flexfit/views/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'views/auth/on_boarding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
